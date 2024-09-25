329cc9cabd83f392924b10967454c9f51af525f5
Yeferson Toquica <yefersontoquica2020@gmail.com>
2024-09-25 17:07:18 -0500

    POST y GET, creando nuestra primer propiedad en la BD

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,14 +1,13 @@
 <?php 
-     function conectarDB() : mysqli {
-        $db = mysqli_connect( 'localhost', 'root', 'toki', 'bienesraices_crud');
-     
+function conectarDB() : mysqli {
+   $db = mysqli_connect( 'localhost', 'root', 'toki', 'bienesraices_crud');
 
-     if(!$db){
-        echo "Error no se pudo conectar";
-        exit;
-     }
+   if(!$db){
+      echo "Error no se pudo conectar";
+      exit;
+   }
 
-     return $db;
+   return $db;
 }
 
 ?>
