8a3c510f7ad70e9e03cf5b015f162547430676e9
Yeferson Toquica <yefersontoquica2020@gmail.com>
2024-09-24 21:52:56 -0500

    Conectando la base de datos

---
+++ includes/config/database.php
@@ -0,0 +1,14 @@
+<?php 
+     function conectarDB() : mysqli {
+        $db = mysqli_connect( 'localhost', 'root', 'toki', 'bienesraices_crud');
+     
+
+     if(!$db){
+        echo "Error no se pudo conectar";
+        exit;
+     }
+
+     return $db;
+}
+
+?>
