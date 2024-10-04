5adb6e24964da1308329b802791e36a3844695e1
Yeferson Toquica <yefersontoquica2020@gmail.com>
2024-10-03 22:10:35 -0500

    Listando las propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,19 @@
 <?php
+    //Importar la conexión
+    require '../includes/config/database.php';
+    $db =conectarDB();
+
+    //Escribir el Query
+    $query = "SELECT * FROM propiedades";
+
+    //Consultar la BD
+    $resultadoConsulta = mysqli_query($db, $query);
 
 
-
+    //Muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
 
+    //Incluye un template
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -27,21 +37,26 @@
                 </tr>
             </thead>
 
-            <tbody>
+            <tbody><!-- Mostrar los resultados -->
+                <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
                 <tr>
-                    <td>1</td>
-                    <td>Casa en la playa</td>
-                    <td><img src="/imagenes/1b935be524bc5b55a2f94eba355c0334.jpg" class="imagen-tabla"></td>
-                    <td>$120000</td>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"></td>
+                    <td>$ <?php echo $propiedad['precio']; ?></td>
                     <td>    
                         <a href="#" class="boton-rojo-block">Eliminar</a>
                         <a href="#" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
+                <?php endwhile;  ?>
             </tbody>
         </table>
     </main>
     
 <?php 
+
+    //Cerrar la conexión(opcional)
+    mysqli_close($db);
     incluirTemplate('footer'); 
 ?>
